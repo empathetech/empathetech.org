554d1131ad8add3de02cc0f9541310a129a3cf32
John4850 <johnnelson4850@gmail.com>
2023-05-26 14:51:25 -0700

    CodeOfConduct setup

---
+++ src/components/codeOfConduct/CodeOfConduct.tsx
@@ -0,0 +1,51 @@
+import React from "react"
+
+export const CodeOfConduct: React.FC = () => {
+  return (
+    <>
+      <ul>
+        <li>
+          <b>
+            {"Respect:"}
+          </b>
+          {"We respect each other as peers, colleagues, and collaborators."}
+        </li>
+
+        <li>
+          <b>
+            {"Self-care:"}
+          </b>
+          {"We allow ourselves to celebrate, and allow ourselves to fail."}
+        </li>
+
+        <li>
+          <b>
+            {"Community:"}
+          </b>
+          {"We are invested in helping each other and sharing knowledge."}
+        </li>
+
+        <li>
+          <b>
+            {"Growth:"}
+          </b>
+          {"We offer honest and constructive feedback, not judgment."}
+        </li>
+
+        <li>
+          <b>
+            {"Dedication:"}
+          </b>
+          {"We work hard, but we don’t presume what that means."}
+        </li>
+
+        <li>
+          <b>
+            {"Integrity:"}
+          </b>
+          {"We bring our lessons to our interviews, not our interviews to our lessons."}
+        </li>
+      </ul>
+    </>
+  )
+}
