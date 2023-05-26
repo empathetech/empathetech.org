b6f424ea3cfd287f9ff00a5762636981fe1e5740
John4850 <johnnelson4850@gmail.com>
2023-05-26 15:27:09 -0700

    Home link

--- src/pages/code-of-conduct.tsx
+++ src/pages/code-of-conduct.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import styles from "@/styles/page.module.css"
 import { TextBlock, CodeOfConduct } from "@/components"
+import Link from 'next/link';
 
 export default function codeOfConduct() {
   return (
     <>
+      <Link href="/">Home</Link>
       <TextBlock>
         <CodeOfConduct />
       </TextBlock>
