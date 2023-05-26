9bc7cc03afafce01916ca2064f062179da979f59
Molly Jean Bennett <mollyjeanbennett@gmail.com>
2023-05-25 20:04:13 -0700

    [F] Implement header, update README

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,14 +2,13 @@ import React from "react"
 import styles from "./header.module.css"
 
 type PropsType = {
-	title: string
 	subTitle?: string
 }
 
-export const Header: React.FC<PropsType> = ({ subTitle, title }) => {
+export const Header: React.FC<PropsType> = ({ subTitle }) => {
 	return (
 		<div className={styles.wrapper}>
-			<h1 className={styles.title}>{title}</h1>
+			<h1 className={styles.title}>Empathetech</h1>
 			{subTitle && <p className={styles.subTitle}>{subTitle}</p>}
 		</div>
 	)
